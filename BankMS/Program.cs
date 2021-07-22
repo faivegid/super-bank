339436b62c6885b1a344b41e06d3ec329600ecb1
faivegid <faivegid@gmail.com>
2021-07-22 21:54:01 +0100

    First commit with empty project

---
+++ BankMS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+/// <summary>
+/// Bank Management System
+/// </summary>
+namespace BankMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bank Management System");
+        }
+    }
+}
